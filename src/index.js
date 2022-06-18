b469697f293f60f30d57406099421a0e4b9dfbaa
stefanusricky93@gmail.com <stefanusricky93@gmail.com>
2022-06-19 05:02:25 +0700

    Update root to react18

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 
 //import Post from './component/Post';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
     
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 
